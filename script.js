b1cf963ff529656e1a496c2df4e7d881db7b4f97
Rapeepat Treerattrakul <rapeepat.ptpk@gmail.com>
2025-01-23 10:18:25 +0700

    Search products with category

--- script.js
+++ script.js
@@ -2,49 +2,49 @@ let products = {
     data: [
         {
             productName: "Regular White T-Shirt",
-            category: "Topwear",
+            category: "topwear",
             price: 30,
             image: "./image/white-tshirt.jpg"
         },
         {
             productName: "Beige Short Skirt",
-            category: "Bottomwear",
+            category: "bottomwear",
             price: 49,
             image: "./image/short-skirt.jpg"
         },
         {
             productName: "Sporty SmartWatch",
-            category: "Watch",
+            category: "watch",
             price: 99,
             image: "./image/sporty-smartwatch.jpg"
         },
         {
             productName: "Basic Knitted Top",
-            category: "Topwear",
+            category: "topwear",
             price: 29,
             image: "./image/knitted-top.jpg"
         },
         {
             productName: "Black Leather Jacket",
-            category: "Jacket",
+            category: "jacket",
             price: 129,
             image: "./image/black-leather-jacket.jpg"
         },
         {
             productName: "Stylish Pink Trousers",
-            category: "Topwear",
+            category: "topwear",
             price: 29,
             image: "./image/pink-trousers.jpg"
         },
         {
             productName: "Brown Men's Jacket",
-            category: "Jacket",
+            category: "jacket",
             price: 189,
             image: "./image/brown-jacket.jpg"
         },
         {
             productName: "Comfy Gray Pants",
-            category: "Bottomwear",
+            category: "bottomwear",
             price: 49,
             image: "./image/comfy-gray-pants.jpg"
         },
@@ -55,7 +55,7 @@ for (let i of products.data) {
     console.log('i is', i)
 
     let card = document.createElement("div")
-    card.classList.add("card", "i.category")
+    card.classList.add("card", i.category, "hide")
 
     let imgContainer = document.createElement("div")
     imgContainer.classList.add("image-container")
@@ -80,4 +80,32 @@ for (let i of products.data) {
 
     card.appendChild(container)
     document.getElementById("products").appendChild(card)
+}
+
+window.onload = () => {
+    filterProduct('all')
+}
+
+function filterProduct(value) {
+    let buttons = document.querySelectorAll(".button-value")
+    buttons.forEach((button) => {
+        if (value.toUpperCase() === button.innerText.toUpperCase()) {
+            button.classList.add("active")
+        } else {
+            button.classList.remove("active")
+        }
+    })
+
+    let elements = document.querySelectorAll(".card")
+    elements.forEach((element) => {
+        if (value == "all") {
+            element.classList.remove("hide")
+        } else {
+            if (element.classList.contains(value)) {
+                element.classList.remove("hide")
+            } else {
+                element.classList.add("hide")
+            }
+        }
+    })
 }
